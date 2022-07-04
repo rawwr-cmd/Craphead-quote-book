356c358e79ef9250851533fadfe630f9dedbba74
rawwr <akshayace0302@gmail.com>
2022-07-04 21:24:23 +0530

    adding dummy data and more context

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "author1", text: "text1" },
+  { id: "q2", author: "author2", text: "text2" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
