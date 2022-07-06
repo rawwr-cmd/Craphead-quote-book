a90dfc0d21b5eaeb263e2f06a36443807dc3df71
rawwr <akshayace0302@gmail.com>
2022-07-07 00:01:09 +0530

    adding the comments features

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,21 +1,40 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 import classes from "./NewCommentForm.module.css";
+import useHttp from "../hooks/use-Http";
+import { addComment } from "../hooks/firebaseApi.js";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const NewCommentForm = (props) => {
-  // console.log(props);
   const commentTextRef = useRef();
+
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
+    const enteredText = commentTextRef.current.value;
+
     // optional: Could validate here
 
-    // send comment to server
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor="comment">Your Comment</label>
         <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
